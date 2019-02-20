5e8adb101caa4a879df6d71869eb036d3824ebf5
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-02-20 17:23:30 +0200

    Why did the functional component feel lost? Because it didn't know what state it was in

---
+++ albums/index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+
+// Render it to the device
+AppRegistry.registerComponent(appName, () => App);
