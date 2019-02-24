de68e26207f86fb2727476c0f945aa3566890688
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-02-24 19:15:48 +0200

    Added password input with amazing style

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,25 +1,45 @@
 import React, { Component } from "react";
+import firebase from "firebase";
 import { Button, Card, CardSection, Input } from "../../index.js";
 
 class LoginForm extends Component {
-  state = { text: "" };
+  state = { email: "", password: "" };
+
+  // Authenticate user
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password);
+  }
 
   render() {
     return (
       <Card>
         <CardSection>
           <Input
+            placeholder="user@gmail.com"
+            label="Email"
             // Props value & onChangeText
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })} // text: text
+            value={this.state.email}
+            // {email => } argument can be named on the fly
+            onChangeText={email => this.setState({ email })} // email:email
             // style={{ height: 40, width: 100 }}
           />
         </CardSection>
-
-        <CardSection />
+        {/* Password input */}
+        <CardSection>
+          <Input
+            // Listing this gives automatically value true
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
 
         <CardSection>
-          <Button>Log in</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
         </CardSection>
       </Card>
     );
--- auth/src/components/common/Input.js
+++ auth/src/components/common/Input.js
@@ -3,17 +3,56 @@
 import React, { Component } from "react";
 import { TextInput, Text, View } from "react-native";
 
-const Input = ({ label, value, onChangeText }) => {
+const Input = ({
+  label,
+  value,
+  onChangeText,
+  placeholder,
+  secureTextEntry
+}) => {
+  const { inputStyle, labelStyle, containerStyle } = styles;
+
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={containerStyle}>
+      <Text style={labelStyle}>{label}</Text>
       <TextInput
+        // Password will be obstructed
+        secureTextEntry={secureTextEntry}
+        placeholder={placeholder}
+        autoCorrect={false}
+        style={inputStyle}
         value={value}
         onChangeText={onChangeText}
-        style={{ height: 40, width: 100 }}
       />
     </View>
   );
+};
+
+const styles = {
+  inputStyle: {
+    color: "#000",
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    // Space between each line of text
+    lineHeight: 23,
+    // Spacing
+    flex: 2
+  },
+  labelStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+    flex: 1
+  },
+  containerStyle: {
+    height: 40,
+    // Container style fills all the available space (View)
+    flex: 1,
+    // So that the label and input will lineup in a row direction
+    flexDirection: "row",
+    // Lined up vertically
+    alignItems: "center"
+  }
 };
 
 export { Input };
