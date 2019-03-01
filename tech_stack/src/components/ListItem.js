f3f8e87d6ef64091da53f68b4279b352da5f888d
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-03-01 19:15:35 +0200

    Added animations

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,17 +1,36 @@
 // Logic for LibraryList.js how to show one item
 import React, { Component } from "react";
 // import * as components from 'react-native
-import { Text, TouchableWithoutFeedback, View } from "react-native";
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  NativeModules,
+  LayoutAnimation
+} from "react-native";
 import { connect } from "react-redux";
 import { CardSection } from "../components/common/CardSection";
 // Importing many actions (gives everything that is exported)
 import * as actions from "../actions/index";
 
+const { UIManager } = NativeModules;
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
   // If library id matches with selected library id then it shows description
   renderDescription() {
-    if (this.props.library.item.id === this.props.selectedLibraryId) {
-      return <Text>{this.props.library.item.description}</Text>;
+    if (this.props.expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>{this.props.library.item.description}</Text>
+        </CardSection>
+      );
     }
   }
   render() {
@@ -41,8 +60,10 @@ const styles = {
   }
 };
 
-const mapStateToProps = state => {
-  return { selectedLibraryId: state.selectedLibraryId };
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.item.id;
+  // Key & value is expanded
+  return { expanded };
 };
 // First argument is for mapStateToProps
 // Second argument is for binding action creaters to this component
