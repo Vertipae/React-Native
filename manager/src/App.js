a78ee91b68f2f3a5f787d482798a60c68f8d39b9
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-03-02 15:29:19 +0200

    Added boilerplate

---
+++ manager/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import firebase from "@firebase/app";
+import reducers from "./reducers";
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyC8zO6V8y8k1ZZQp_QTqQkIjbDPTjdhuZA",
+      authDomain: "manager-7e6f2.firebaseapp.com",
+      databaseURL: "https://manager-7e6f2.firebaseio.com",
+      projectId: "manager-7e6f2",
+      storageBucket: "manager-7e6f2.appspot.com",
+      messagingSenderId: "1019094691913"
+    };
+    firebase.initializeApp(config);
+  }
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text> textInComponent </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
