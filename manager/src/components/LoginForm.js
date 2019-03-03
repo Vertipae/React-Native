fe9dbb1296effb17c21e021cbc79915b910389dc
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-03-03 14:45:04 +0200

    Added LoginForm states

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -6,6 +6,7 @@ import { Card } from "./common/Card";
 import { CardSection } from "./common/CardSection";
 import { Input } from "./common/Input";
 import { Button } from "./common/Button";
+import { Spinner } from "./common/Spinner";
 // import console = require("console");
 
 class LoginForm extends Component {
@@ -21,6 +22,24 @@ class LoginForm extends Component {
     const { email, password } = this.props.auth;
 
     this.props.loginUser({ email, password });
+  }
+
+  renderError() {
+    if (this.props.auth.error) {
+      return (
+        <View style={{ backgroundColor: "white" }}>
+          <Text style={styles.errorTextStyle}>{this.props.auth.error}</Text>
+        </View>
+      );
+    }
+  }
+
+  renderButton() {
+    if (this.props.auth.loading) {
+      return <Spinner size="large" />;
+    }
+
+    return <Button onPress={this.onButtonPress.bind(this)}>Login</Button>;
   }
 
   render() {
@@ -45,13 +64,22 @@ class LoginForm extends Component {
           />
         </CardSection>
 
-        <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
-        </CardSection>
+        {this.renderError()}
+
+        <CardSection>{this.renderButton()}</CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: "center",
+    color: "red"
+  }
+};
+
 const mapStateToProps = state => ({
   auth: state.auth
 });
--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,10 +1,19 @@
 // First thing to look at when getting undefined error
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from "../actions/types";
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+  LOGIN_USER
+} from "../actions/types";
 // import console = require("console");
 
 const INITIAL_STATE = {
   email: "",
-  password: ""
+  password: "",
+  user: null,
+  error: "",
+  loading: false
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -12,7 +21,6 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
       //   console.log("action!");
-
       return {
         // Make a new object that takes all of the properties of existing state object and throw them into that object
         // Then define property email given the value of action.payload and toss it to ...state (Example if someone gives value "asd" to email input)
@@ -23,6 +31,30 @@ export default (state = INITIAL_STATE, action) => {
       return {
         ...state,
         password: action.payload
+      };
+    case LOGIN_USER:
+      return {
+        ...state,
+        loading: true,
+        error: ""
+      };
+    case LOGIN_USER_SUCCESS:
+      return {
+        ...state,
+        ...INITIAL_STATE,
+        user: action.payload
+        // error: "",
+        // loading: false,
+        // email: "",
+        // password: ""
+      };
+    case LOGIN_USER_FAIL:
+      return {
+        ...state,
+        error: "Authentication Failed",
+        // Clearing password when user gets error
+        password: "",
+        loading: false
       };
     default:
       return state;
