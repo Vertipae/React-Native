4ee199845fe6378cfe597a9ca6aeed98aa5b1e70
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-03-03 18:06:07 +0200

    Added navigation between routes

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
 import { Scene, Router } from "react-native-router-flux";
 import LoginForm from "./components/LoginForm";
+import EmployeeList from "./components/EmployeeList";
 // import { Text, View } from 'react-native'
 
 const RouterComponent = () => {
   return (
     <Router>
       {/* Root/Parent Scene */}
-      <Scene key="root">
-        <Scene
-          key="login"
-          component={LoginForm}
-          title="Please Login"
-          titleStyle={style.titleStyle}
-        />
+      <Scene key="root" titleStyle={style.titleStyle}>
+        {/* Initial = First component to show */}
+        <Scene key="login" component={LoginForm} title="Please Login" initial />
+        <Scene key="employeeList" component={EmployeeList} title="Employees" />
       </Scene>
     </Router>
   );
